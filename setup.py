7d2062903e673300e398ce77d681c0ee66949382
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-08-19 19:06:40 +0100

    Release 0.0.7.

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ add_cython_extension('fastbencode._bencode_pyx')
 setup(
     name="fastbencode",
     description="Implementation of bencode with optional fast C extensions",
-    version="0.0.6",
+    version="0.0.7",
     maintainer="Breezy Developers",
     maintainer_email="breezy-core@googlegroups.com",
     url="https://github.com/breezy-team/fastbencode",
