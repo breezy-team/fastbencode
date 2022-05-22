71017aa365543ab1bf1e876e9dac25f5a6dbf539
Jelmer Vernooĳ <jelmer+releaser@jelmer.uk>
2022-05-22 20:01:58 +0000

    Release 0.0.9.

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ add_cython_extension('fastbencode._bencode_pyx')
 setup(
     name="fastbencode",
     description="Implementation of bencode with optional fast C extensions",
-    version="0.0.8",
+    version="0.0.9",
     maintainer="Breezy Developers",
     maintainer_email="breezy-core@googlegroups.com",
     url="https://github.com/breezy-team/fastbencode",
